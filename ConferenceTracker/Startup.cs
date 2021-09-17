38fdd105fcd2c0fd56925eb25d09fe6e9e449393
Stephen McCauley <stephen.mccauley@allenovery.com>
2021-09-17 13:26:22 +0100

    amended dev check

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -55,16 +55,16 @@ namespace ConferenceTracker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            if(!env.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-            else
+            if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
                 logger.LogInformation("Environment is in development");
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
